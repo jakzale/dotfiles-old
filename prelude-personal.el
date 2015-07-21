e86ec60d3924756c322d38a22649694e9e3dc082
Jakub Z <jakzale@gmail.com>
2015-07-21 02:04:39 +0100

    Add org-trello mode for *.trello files

--- prelude-personal.el
+++ prelude-personal.el
@@ -1,3 +1,15 @@
 ;; Prelude personal configuration
 
-;; Empty for now
+;; Load org mode and trello integration
+(require 'org-trello)
+
+;; Trello org-mode configuration
+
+;; recognise *.trello files ase org-mode files
+(add-to-list 'auto-mode-alist '("\\.trello$" . org-mode))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (let ((fname (buffer-file-name (current-buffer))))
+              (when (and fname (string= "trello" (file-name-extension fname)))
+                (org-trello-mode)))))
