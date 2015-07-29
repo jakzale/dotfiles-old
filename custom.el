bc7dbdefb44dfdce473f77bf5ad201abfd9234fd
Jakub Z <jakzale@gmail.com>
2015-07-29 11:39:24 +0100

    Move personal configuration to custom.el

--- custom.el
+++ custom.el
@@ -1,3 +1,22 @@
+;;;; My own custom configuration
+;; Prelude personal configuration
+
+;; Load org mode and trello integration
+(require 'org-trello)
+
+;; Trello org-mode configuration
+
+;; recognise *.trello files ase org-mode files
+(add-to-list 'auto-mode-alist '("\\.trello$" . org-mode))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (let ((fname (buffer-file-name (current-buffer))))
+              (when (and fname (string= "trello" (file-name-extension fname)))
+                (org-trello-mode)))))
+
+;;;; Custom set variables
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
