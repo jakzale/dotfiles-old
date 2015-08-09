d0a154bccecc98631d5aa858daee0fff3cf56168
Jakub Z <jakzale@gmail.com>
2015-08-09 21:37:52 +0100

    Add done command to eshell

--- custom.el
+++ custom.el
@@ -97,6 +97,15 @@
                                (interactive)
                                (my-swap-theme)))
 
+
+;; Add done command to eshell
+(require 'alert)
+
+(defun eshell/done ()
+  "A simple function that will notify when done"
+  (alert "Done!" :persistent t))
+
+
 ;;;; Custom set variables
 
 (custom-set-variables
@@ -104,6 +113,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(alert-default-style (quote notifier))
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
