5d14e90393679af8918b7fcf2decf1c67c905149
Jakub Z <jakzale@gmail.com>
2016-10-04 11:12:55 +0100

    (emacs) start working on emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,22 @@
 ;; Really Basic Emacs file :p
+
+(require 'package)
+
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/")
+	     t)
+
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (geiser))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
