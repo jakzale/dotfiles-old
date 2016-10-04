4bd1d27326e53ca88bc12e1fb77fca5a79a32292
Jakub Z <jakzale@gmail.com>
2016-10-04 11:34:29 +0100

    Add zenburn theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,7 +18,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (geiser))))
+ '(custom-safe-themes
+   (quote
+    ("0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856" default)))
+ '(package-selected-packages (quote (zenburn-theme geiser))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
